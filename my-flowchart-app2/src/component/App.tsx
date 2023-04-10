a95e013f08a5b5de4c8026a52c95707ac451f618
NK-shinonomesora <miotsumugii@gmail.com>
2023-04-10 14:25:05 +0900

    init

---
+++ my-flowchart-app2/src/component/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <p>This is App Component.</p>
+  )
+}
+
+export default App;
---
+++ my-flowchart-app2/src/renderer.tsx
@@ -0,0 +1,10 @@
+import './index.css';
+import React from 'react';
+import { createRoot } from "react-dom/client"
+import App from './component/App';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <App />
+)
